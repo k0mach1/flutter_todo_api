e79e8b6df7f065e0482874ef23a20551f7bb2064
mochisan <info@mochida.me>
2022-11-06 16:30:58 +0900

    todosを返すAPI

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type Todo struct {
+	Title  string `json:"title"`
+	IsDone bool   `json:"is_done"`
+}
+
+var (
+	todos []Todo
+)
+
+func main() {
+	todos = append(todos, Todo{
+		Title:  "todo 1",
+		IsDone: false,
+	})
+	todos = append(todos, Todo{
+		Title:  "todo 2",
+		IsDone: false,
+	})
+	todos = append(todos, Todo{
+		Title:  "todo 3",
+		IsDone: false,
+	})
+
+	http.HandleFunc("/", getTodos)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(todos)
+	case "POST":
+	default:
+		fmt.Fprint(w, "Method not allowed.\n")
+	}
 }
